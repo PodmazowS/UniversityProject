8c3bb6b6e7720b204aa7c7a953225c2b8a33ad6d
PodmazowS <podmazowstas@gmail.com>
2023-01-07 15:06:43 +0100

    add repositories(interfaces)

---
+++ ShoppingBasket.DataAccess/Repositories/IAssignment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingBasket.DataAccess.Repositories
+{
+    internal interface IAssignment
+    {
+        IProductR Product { get; }
+        ICategoryR Category { get; }
+        void Save();
+    }
+}
