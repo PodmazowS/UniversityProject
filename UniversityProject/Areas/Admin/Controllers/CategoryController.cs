dde69fb31171eea528026c3486206198c6faf21d
PodmazowS <podmazowstas@gmail.com>
2023-01-12 23:14:04 +0100

    update category controller, add create and delete operations and add view

--- UniversityProject/Areas/Admin/Controllers/CategoryController.cs
+++ UniversityProject/Areas/Admin/Controllers/CategoryController.cs
@@ -21,5 +21,72 @@ namespace UniversityProject.Web.Areas.Admin.Controllers
             categoryVM.categories = _assignment.Category.GetAll();
             return View(categoryVM);
         }
+        [HttpGet]
+        public IActionResult CreateUpdate(int? id)
+        {
+            CategoryViewModels vm = new CategoryViewModels();
+            if (id == null || id == 0) {
+                return View(vm);
+            }
+            else
+            {
+                vm.Category = _assignment.Category.GetT(x => x.Id == id);
+                if (vm.Category == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return View(vm);
+                }
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateUpdate(CategoryViewModels vm)
+        {
+            if (ModelState.IsValid)
+            {
+                if(vm.Category.Id == 0) {
+                    _assignment.Category.Add(vm.Category);
+                    TempData["success"] = "Category Created";
+                }
+                else {
+                    _assignment.Category.Update(vm.Category);
+                    TempData["success"] = "Category Updated";
+                }
+                _assignment.Save();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+
+        }
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _assignment.Category.GetT(x => x.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteData(int? id) {
+            var category = _assignment.Category.GetT(x => x.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _assignment.Category.Delete(category);
+            _assignment.Save();
+            TempData["success"] = "Category Deleted";
+            return RedirectToAction("Index");
+        }
     }
 }
